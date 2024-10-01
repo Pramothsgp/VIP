343452ad9ccb9cff6e477c41542042fc1ab1d953
Pramothsgp <jarvis.tech.006@mail.com>
2024-10-01 18:19:56 +0530

    Querry Written for filtering jobs and improvised css

--- backend/Routes/Jobs/getJobs.js
+++ backend/Routes/Jobs/getJobs.js
@@ -1,26 +1,65 @@
-const db = require('../../config/db');
+const db = require("../../config/db");
 
 const getJobs = (req, res) => {
-    const query = 'SELECT * FROM jobs';
-    db.query(query, (err, result) => {
-        if (err) {
-            console.error('Error fetching jobs:', err);
-            res.status(500).json({ error: 'Failed to fetch jobs' });
-        } else {
-            // Convert BLOB data to Base64 string for each job
-            // const jobsWithBase64Images = result.map(job => {
-            //     if (job.imagedata) {
-            //         job.imagedata = Buffer.from(job.imagedata).toString('base64');
-            //     }
-                
-            //     return job;
-            // });
-            console.log(result);   
-            res.status(200).json(result);
-        }
-    });
+  const query = "SELECT * FROM jobs";
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error("Error fetching jobs:", err);
+      res.status(500).json({ error: "Failed to fetch jobs" });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 };
 
+const getSpeceficJobs = (req, res) => {
+  const { job, jobSetup, salaryRange } = req.body;
+  if (job === "All" && jobSetup === "All") {
+    const query = "SELECT * FROM jobs WHERE salary BETWEEN ? AND ?";
+    db.query(query, [...salaryRange], (err, result) => {
+      if (err) {
+        console.error("Error fetching jobs:", err);
+        res.status(500).json({ error: "Failed to fetch jobs" });
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  } else if (job === "All" && jobSetup !== "All") {
+    const query =
+      "SELECT * FROM jobs WHERE setup = ? AND salary BETWEEN ? AND ?";
+    db.query(query, [jobSetup, ...salaryRange], (err, result) => {
+      if (err) {
+        console.error("Error fetching jobs:", err);
+        res.status(500).json({ error: "Failed to fetch jobs" });
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  } else if (job !== "All" && jobSetup === "All") {
+    const query =
+      "SELECT * FROM jobs WHERE jobType = ? AND salary BETWEEN ? AND ?";
+    db.query(query, [job, ...salaryRange], (err, result) => {
+      if (err) {
+        console.error("Error fetching jobs:", err);
+        res.status(500).json({ error: "Failed to fetch jobs" });
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  } else if (job !== "All" && jobSetup !== "All") {
+    const query =
+      "SELECT * FROM jobs WHERE jobType = ? AND setup = ? AND salary BETWEEN ? AND ?";
+    db.query(query, [job, jobSetup, ...salaryRange], (err, result) => {
+      if (err) {
+        console.error("Error fetching jobs:", err);
+        res.status(500).json({ error: "Failed to fetch jobs" });
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  }
+};
 module.exports = {
-    getJobs
+    getJobs,
+    getSpeceficJobs
 };
--- vip-frontend/src/Pages/Addjob/AddJob.jsx
+++ vip-frontend/src/Pages/Addjob/AddJob.jsx
@@ -1,100 +1,194 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-
+import axios from "axios";
+import React, { useState } from "react";
+import "./Addjob.css";
+import { toast } from "react-toastify";
 const AddJob = () => {
-    const [jobData, setJobData] = useState({
-        title: '',
-        location: '',
-        setup: '',
-        salary: '',
-        description: '',
-        companyName: '',
-        qualification: '',
-        responsibilities: '',
-        projects: '',
-        jobType: '',
-        imagedata: null,
-    });
-    
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        // Create FormData object to handle file upload
-        const formData = new FormData();
-        Object.keys(jobData).forEach((key) => {
-            formData.append(key, jobData[key]);
-        });
-        
-        try {
-            const response = await axios.post('http://localhost:5000/api/addjob', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            });
-            console.log(response.data);
-        } catch (error) {
-            console.error('Error adding job:', error);
-        }
-    };
+  const jobTypes = ["Full-Time", "Part-Time", "Contractual", "Internship"];
+  const jobSetups = ["Remote", "Hybrid", "On-site"];
+  const [jobData, setJobData] = useState({
+    title: "",
+    location: "",
+    setup: "",
+    salary: "",
+    description: "",
+    companyName: "",
+    qualification: "",
+    responsibilities: "",
+    projects: "",
+    jobType: "",
+    imagedata: null,
+  });
 
-    const handleFileChange = (e) => {
-        setJobData({ ...jobData, imagedata: e.target.files[0] }); // Handle file separately
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(jobData);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/addjob",
+        jobData 
+        );
+        toast.success(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error adding job:", error);
+    }
+  };
+
+  const handleFileChange = (e) => {
+    setJobData({ ...jobData, imagedata: e.target.files[0] }); // Handle file separately
+  };
   return (
+    <form className="job-form" onSubmit={handleSubmit}>
       <div>
-          <h1>Add Job</h1>
-          <form className='job-form' onSubmit={handleSubmit}>
-              <div className='form-group'>
-                  <div className='input-group'>
-                      <label htmlFor="title">Title</label>
-                      <input type="text" id="title" name="title" value={jobData.title} onChange={(e) => setJobData({ ...jobData, title: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="location">Location</label>
-                      <input type="text" id="location" name="location" value={jobData.location} onChange={(e) => setJobData({ ...jobData, location: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="setup">Setup</label>
-                      <input type="text" id="setup" name="setup" value={jobData.setup} onChange={(e) => setJobData({ ...jobData, setup: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="salary">Salary</label>
-                      <input type="text" id="salary" name="salary" value={jobData.salary} onChange={(e) => setJobData({ ...jobData, salary: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="description">Description</label> 
-                      <textarea id="description" name="description" value={jobData.description} onChange={(e) => setJobData({ ...jobData, description: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="companyName">Company Name</label>
-                      <input type="text" id="companyName" name="companyName" value={jobData.companyName} onChange={(e) => setJobData({ ...jobData, companyName: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="qualification">Qualification</label>
-                      <input type="text" id="qualification" name="qualification" value={jobData.qualification} onChange={(e) => setJobData({ ...jobData, qualification: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="responsibilities">Responsibilities</label>
-                      <input type="text" id="responsibilities" name="responsibilities" value={jobData.responsibilities} onChange={(e) => setJobData({ ...jobData, responsibilities: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="projects">Projects</label>
-                      <input type="text" id="projects" name="projects" value={jobData.projects} onChange={(e) => setJobData({ ...jobData, projects: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="jobType">Job Type</label>
-                      <input type="text" id="jobType" name="jobType" value={jobData.jobType} onChange={(e) => setJobData({ ...jobData, jobType: e.target.value })} />
-                  </div>
-                  <div className='input-group'>
-                      <label htmlFor="imagedata">Image</label>
-                      <input type="file" id="imagedata" name="imagedata"  onChange={handleFileChange} />
-                  </div>        
-                  <button type="submit">Submit</button>
-              </div>
-          </form>
-          
-    </div>
-  )
-}
+        <h1>Add Job</h1>
+        <div className="form-group">
+          <div className="input-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="title"
+              value={jobData.title}
+              onChange={(e) =>
+                setJobData({ ...jobData, title: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="location">Location</label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              value={jobData.location}
+              onChange={(e) =>
+                setJobData({ ...jobData, location: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="setup">Setup</label>
+            <select
+              id="setup"
+              name="setup"
+              value={jobData.setup}
+              onChange={(e) =>
+                setJobData({ ...jobData, setup: e.target.value })
+              }
+            >
+              <option value="">Select Setup</option>
+              {jobSetups.map((setup, index) => (
+                <option key={index} value={setup}>
+                  {setup}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="input-group">
+            <label htmlFor="salary">Salary</label>
+            <input
+              type="number"
+              id="salary"
+              name="salary"
+              value={jobData.salary}
+              onChange={(e) =>
+                setJobData({ ...jobData, salary: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              name="description"
+              value={jobData.description}
+              onChange={(e) =>
+                setJobData({ ...jobData, description: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="companyName">Company Name</label>
+            <input
+              type="text"
+              id="companyName"
+              name="companyName"
+              value={jobData.companyName}
+              onChange={(e) =>
+                setJobData({ ...jobData, companyName: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="qualification">Qualification</label>
+            <input
+              type="text"
+              id="qualification"
+              name="qualification"
+              value={jobData.qualification}
+              onChange={(e) =>
+                setJobData({ ...jobData, qualification: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="responsibilities">Responsibilities</label>
+            <input
+              type="text"
+              id="responsibilities"
+              name="responsibilities"
+              value={jobData.responsibilities}
+              onChange={(e) =>
+                setJobData({ ...jobData, responsibilities: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="projects">Projects</label>
+            <input
+              type="text"
+              id="projects"
+              name="projects"
+              value={jobData.projects}
+              onChange={(e) =>
+                setJobData({ ...jobData, projects: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="jobType">Job Type</label>
+            <select
+              id="jobType"
+              name="jobType"
+              value={jobData.jobType}
+              onChange={(e) =>
+                setJobData({ ...jobData, jobType: e.target.value })
+              }
+            >
+              <option value="">Select Job Type</option>
+              {jobTypes.map((type, index) => (
+                <option key={index} value={type}>
+                  {type}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="input-group">
+            <label htmlFor="imagedata">Image</label>
+            <input
+              type="file"
+              id="imagedata"
+              name="imagedata"
+              onChange={handleFileChange}
+            />
+          </div>
+          <button type="submit">Add job</button>
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default AddJob
+export default AddJob;
